09a7154fb80c9bd20d57c99363a9a0851efc70b5
Zeeshan Lokhandwala <zalokhan@ncsu.edu>
2018-09-06 12:04:47 -0700

    Exponential back-off before address holefill.

--- runtime/src/main/java/org/corfudb/runtime/view/replication/ReadWaitHoleFillPolicy.java
+++ runtime/src/main/java/org/corfudb/runtime/view/replication/ReadWaitHoleFillPolicy.java
@@ -1,14 +1,22 @@
 package org.corfudb.runtime.view.replication;
 
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
+
 import javax.annotation.Nonnull;
 
 import org.corfudb.protocols.wireprotocol.ILogData;
 import org.corfudb.runtime.exceptions.HoleFillRequiredException;
-import org.corfudb.util.Sleep;
+import org.corfudb.runtime.exceptions.RetryExhaustedException;
+import org.corfudb.runtime.exceptions.unrecoverable.UnrecoverableCorfuInterruptedError;
+import org.corfudb.util.retry.ExponentialBackoffRetry;
+import org.corfudb.util.retry.IRetry;
+import org.corfudb.util.retry.RetryNeededException;
 
 
-/** A hole filling policy which reads several times,
+/**
+ * A hole filling policy which reads several times,
  * waiting a static amount of time in between, before
  * requiring a hole fill.
  *
@@ -16,21 +24,28 @@ import org.corfudb.util.Sleep;
  */
 public class ReadWaitHoleFillPolicy implements IHoleFillPolicy {
 
-    /** The amount of time to wait between reads, in milliseconds. */
-    final int waitMs;
-
-    /** The amount of times to retry before requiring a hole fill. */
-    final int numRetries;
-
-    /** Create a ReadWaitHoleFillPolicy with the given wait times
-     * and retries.
-     * @param waitMs        The amount of time to wait before retrying.
-     * @param numRetries    The number of retries to apply before requiring a
-     *                      hole fill.
+    /**
+     * Maximum duration after which no more read attempts are made and an address hole fill is
+     * attempted.
      */
-    public ReadWaitHoleFillPolicy(int waitMs, int numRetries) {
-        this.waitMs = waitMs;
-        this.numRetries = numRetries;
+    private final Duration maxHoleFillDuration;
+
+    /**
+     * Max wait interval between consequtive read attempts to cap exponential back-off.
+     */
+    private final Duration maxRetryWaitThreshold;
+
+    /**
+     * Create a ReadWaitHoleFillPolicy with the given wait times
+     * and retries.
+     *
+     * @param maxHoleFillDuration   The amount of time to wait before retrying.
+     * @param maxRetryWaitThreshold The number of retries to apply before requiring a
+     *                              hole fill.
+     */
+    public ReadWaitHoleFillPolicy(Duration maxHoleFillDuration, Duration maxRetryWaitThreshold) {
+        this.maxHoleFillDuration = maxHoleFillDuration;
+        this.maxRetryWaitThreshold = maxRetryWaitThreshold;
     }
 
     /**
@@ -40,22 +55,33 @@ public class ReadWaitHoleFillPolicy implements IHoleFillPolicy {
     @Override
     public ILogData peekUntilHoleFillRequired(long address, Function<Long, ILogData> peekFunction)
             throws HoleFillRequiredException {
-        int tryNum = 0;
-        do {
-            // If this is not the first try, sleep before trying again
-            if (tryNum != 0) {
-                Sleep.MILLISECONDS.sleepUninterruptibly(waitMs);
-            }
-            // Try the read
-            ILogData data = peekFunction.apply(address);
-            // If it was not null, we can return it.
-            if (data != null) {
-                return data;
-            }
-            // Otherwise increment the counter and try again.
-            tryNum++;
-        } while (numRetries > tryNum);
+        final AtomicLong startTime = new AtomicLong();
 
-        throw new HoleFillRequiredException("No data after " + tryNum + " retries");
+        try {
+            IRetry.build(ExponentialBackoffRetry.class, () -> {
+
+                // Try the read
+                ILogData data = peekFunction.apply(address);
+                // If it was not null, we can return it.
+                if (data != null) {
+                    return data;
+                }
+
+                if (startTime.get() == 0) {
+                    startTime.set(System.currentTimeMillis());
+                } else if (System.currentTimeMillis() - startTime.get() >= maxHoleFillDuration
+                        .toMillis()) {
+                    throw new RetryExhaustedException("Retries Exhausted.");
+                }
+                // Otherwise try again.
+                throw new RetryNeededException();
+            }).setOptions(x -> x.setMaxRetryThreshold(maxRetryWaitThreshold)).run();
+        } catch (InterruptedException ie) {
+            throw new UnrecoverableCorfuInterruptedError(ie);
+        } catch (RetryExhaustedException ree) {
+            // Retries exhausted. Hole filling.
+        }
+
+        throw new HoleFillRequiredException("No data after " + maxHoleFillDuration.toMillis() + "ms.");
     }
 }
